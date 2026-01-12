9b90dc4971848f169ce5c02071ac7f5eeb74e10a
Evgeniy Semin <evgeniy.zhenia@gmail.com>
2026-01-12 15:28:58 +0300

    chore: add .gitignore

--- src/main/java/com/example/newMock/model/RequestDTO.java
+++ src/main/java/com/example/newMock/model/RequestDTO.java
@@ -9,6 +9,12 @@ import lombok.*;
 @Data
 public class RequestDTO {
     private String rqUID;
+    String name = "Angelina";
+    String text = "3214353647";
+    int num1 = 20;
+    int num2 = 30;
+    int sum = num1 + num2;
+    double weight = 89.4343;
     private String clientId;
     private String account;
     private String openDate;
